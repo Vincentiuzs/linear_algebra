74a0e9798312beaf5ae95843a8eb02fc6a4c125e
vincent <mthabo081@gmail.com>
2022-01-27 01:13:46 +0200

    added constant(), zeros(), ones() and diag()

--- linear_algebra/matrix_functions.py
+++ linear_algebra/matrix_functions.py
@@ -1,10 +1,31 @@
 from linear_algebra.matrix import Matrix
 
 def transpose(matrix):
-
     if isinstance(matrix, Matrix):
         return matrix.transpose()
-
     return None
 
+def constant(k, nrows, ncols):
+    array = [[k for j in range(ncols)] for i in range(nrows)]]
+    return Matrix(array)
 
+def zeros(nrows, ncols):
+    return constant(0, nrows, ncols)
+
+def ones(nrows, ncols):
+    return constant(1, nrows, ncols)
+
+def diag(matrix):
+
+    if isinstance(matrix, Matrix):
+        if matrix.is_square():
+            n = matrix.p
+            array = zeros(n, n)
+
+            for i in range(n):
+                array[i][i] == 1
+
+            return Matrix(array)
+            
+def inverse(matrix):
+    pass
