70b3092d4bf38e07c9ede77f5d1012161a28f0ab
vincent <mthabo081@gmail.com>
2022-01-27 10:45:20 +0200

    added indentity(), diagonal()

--- linear_algebra/matrix_functions.py
+++ linear_algebra/matrix_functions.py
@@ -1,4 +1,4 @@
-from linear_algebra.matrix import Matrix
+from matrix import Matrix
 
 def transpose(matrix):
     if isinstance(matrix, Matrix):
@@ -6,7 +6,7 @@ def transpose(matrix):
     return None
 
 def constant(k, nrows, ncols):
-    array = [[k for j in range(ncols)] for i in range(nrows)]]
+    array = [[k for j in range(ncols)] for i in range(nrows)]
     return Matrix(array)
 
 def zeros(nrows, ncols):
@@ -15,17 +15,32 @@ def zeros(nrows, ncols):
 def ones(nrows, ncols):
     return constant(1, nrows, ncols)
 
-def diag(matrix):
+def diagonal(matrix):
 
     if isinstance(matrix, Matrix):
         if matrix.is_square():
             n = matrix.p
-            array = zeros(n, n)
+            array = zeros(n, n).matrix
 
             for i in range(n):
-                array[i][i] == 1
+                array[i][i] = matrix[i, i]
 
             return Matrix(array)
-            
+
+def diag(*elements):
+
+    n = len(elements)
+    array = zeros(n, n).matrix
+    for i in range(n):
+        array[i][i] = elements[i]
+    
+    return Matrix(array)
+
+def identity(n):
+    return diag(*[1 for i in range(n)])
+
 def inverse(matrix):
     pass
+
+print (identity(12))
+
